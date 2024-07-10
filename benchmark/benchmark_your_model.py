cb4bba3c2a5756582e2da5c77765d0b0763a3ea6
NanyiCc <cz990816@gmail.com>
2024-07-10 21:09:30 +0800

    benchmark bug fix

--- benchmark/benchmark_your_model.py
+++ benchmark/benchmark_your_model.py
@@ -1,18 +1,18 @@
-import os, json
+import os, json, re
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from tqdm import tqdm
 def multichoice(model_name):
-    QnA_dir_path = '/data/benchmark/final_benchmark_dataset.json'
+    QnA_dir_path = '/home/chenzan/workSpace/data/benchmark/ProteinLMBench.json'
     with open(QnA_dir_path, 'r') as f:
         file_data = json.load(f)
 
-    model_path = f'/data/llm_models/{model_name}'
+    model_path = f'/you_models_parent_path/{model_name}'
     if 'models--' in model_name:
         fs = f'/data/llm_models/huggingface/hub/{model_name}/snapshots/'
         model_path = fs + os.listdir(f'/data/llm_models/huggingface/hub/{model_name}/snapshots/')[0]
     tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
     model = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True, device_map = "auto").eval()
 
-    import re
     answer_list = [f['answer'] for f in file_data]
     answer_list = [re.search(r'\d+', a).group() for a in answer_list]
 
@@ -22,36 +22,25 @@ If you are still unsure about the answer, output option 7.
 Select only ONE correct option by its number. Start your response with 'The correct option is' followed by the option number ONLY. eg: "The correct option is Option X."
 Think step by step.
     """)
+
     question = []
-
-
-    for f in file_data:
+    for f in file_data[:]:
         options = ''
         for o in f['options']:
             options += o + '\n'
         sb = prompt + '\n Question: \n' + f['question'] + '\n Options: \n' + options + '\nThe correct option is:'
         question.append(sb)
 
-    inputs = []
     tokenizer.pad_token = tokenizer.eos_token
-    for q in question:
-    # for q in question:
-    #     a = tokenizer.apply_chat_template([{"role": "user", "content": q}], return_tensors="pt").to("cuda")
-        a = tokenizer(q, return_tensors="pt", padding=True)
-        input_ids = a.input_ids.to('cuda')
-        inputs.append(input_ids)
-
-    print(len(inputs))
 
     chat_model = ('chat' in model_name) or ('Chat' in model_name)
     if 'Yi' in model_name:
         chat_model = False
-    from tqdm import tqdm
-    output_list = []
 
+    output_list = []
     temp = 0.1
     mnt = 20
-    for q in tqdm(inputs[:]):
+    for q in tqdm(question[:]):
         if chat_model:
             try:
                 if 'Mistral' in model_name:
@@ -61,12 +50,18 @@ Think step by step.
             except:
                 output_list.append(model.generate(q, max_new_tokens=mnt, do_sample=True, temperature=temp))
         else:
+            
+            a = tokenizer(q, return_tensors="pt", padding=True)
+            input_ids = a.input_ids.to('cuda')
             if 'Mistral' in model_name:
-                output_list.append(model.generate(q, max_new_tokens=mnt,do_sample=True, temperature=temp, eos_token_id=2, pad_token_id=2))
+                output_list.append(model.generate(input_ids, max_new_tokens=mnt,do_sample=True, temperature=temp, eos_token_id=2, pad_token_id=2))
             else:
-                output_list.append(model.generate(q, max_new_tokens=mnt, do_sample=True,temperature=temp))
-
-    lst = [tokenizer.decode(i[0], skip_special_tokens=True) for i in output_list]
+                output_list.append(model.generate(input_ids, max_new_tokens=mnt, do_sample=True,temperature=temp))
+    # print(output_list[1])
+    try:     
+        lst = [tokenizer.decode(i[0], skip_special_tokens=True) for i in output_list]
+    except:
+        lst = [i[0] for i in output_list]
 
 
     after = []
@@ -85,14 +80,21 @@ Think step by step.
             non_number += 1
             v_ans.append("None")
 
-    print(non_number)
+    print(f"The number of answer we could find is {non_number}.")
     psd = 0
     # wrong_list = []
     from datetime import datetime
     now = datetime.now()
     formatted_time = now.strftime("%Y%m%d_%H%M%S")
     if "/" in model_name:
-        model_name = model_name.split("/")[2]
+        model_name_list = model_name.split("/")
+        if model_name_list[-1] == "":
+            model_name = model_name_list[-2]
+        else:
+            model_name = model_name_list[-1]
+
+    if not os.path.exists("result/"):
+        os.makedirs("result/")
 
     with open(f'result/raw_result_{formatted_time}_{model_name}.json', 'w') as jj:
         json.dump(after, jj)
@@ -108,22 +110,21 @@ Think step by step.
                 results.write("Right")
                 psd+=1
                 results.write("\n")
-
     accuracy = psd/len(v_ans)
     print('correct rate: ' + str(psd / len(v_ans)))
+
 
     del tokenizer
     del model
     return accuracy
 
 model_name_list = [
-    'your_model1',
-    'your_model2'
+    'chat',
+    'test'
 ]
 for model in model_name_list:
-    try:
-        acc = multichoice(model)
-        print(f"Acc of {model} is: {acc}")
-    except Exception as e:
-        continue
+    
+    acc = multichoice(model)
+    print(f"Acc of {model} is: {acc}")
+
 
